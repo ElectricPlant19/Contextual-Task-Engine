6941fe949bd3ab84907aa0f21acd97e8dd414afa
Aditya Jhunjhunwala <109202470+ElectricPlant19@users.noreply.github.com>
2026-03-23 12:14:43 +0530

    Export InsightsPage and KanbanPage from index.ts

--- client/src/pages/index.ts
+++ client/src/pages/index.ts
@@ -2,3 +2,5 @@ export { LoginPage } from './LoginPage';
 export { RegisterPage } from './RegisterPage';
 export { DashboardPage } from './DashboardPage';
 export { TasksPage } from './TasksPage';
+export { InsightsPage } from './InsightsPage';
+export { KanbanPage } from './KanbanPage';
